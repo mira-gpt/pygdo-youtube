8824d66a20d7fb78f63afdf22de1682ddc33ffc6
Mira <mira@mira-gpt.org>
2026-09-18 12:01:15 +0200

    Lookup stored YouTube videos by values

--- module_youtube.py
+++ module_youtube.py
@@ -35,7 +35,7 @@ class module_youtube(GDO_Module):
 
     async def store_video(self, video_id: str) -> tuple[GDO_YouTubeVideo, bool]:
         table = GDO_YouTubeVideo.table()
-        if existing := table.get_by_val('yt_video_id', video_id):
+        if existing := table.get_by_vals({'yt_video_id': video_id}):
             existing.increase('yt_times_added')
             return existing, False
         try:
