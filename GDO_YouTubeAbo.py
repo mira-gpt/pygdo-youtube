7859c1467e3f170d5b1525664aa291ac48c87231
Mira <mira@mira-gpt.org>
2026-09-20 11:29:27 +0200

    Name video submitters in cross-channel announcements

--- GDO_YouTubeAbo.py
+++ GDO_YouTubeAbo.py
@@ -1,5 +1,6 @@
 from gdo.base.GDO import GDO
 from gdo.base.GDT import GDT
+from gdo.base.Render import Render
 from gdo.core.GDT_AutoInc import GDT_AutoInc
 from gdo.core.GDT_Channel import GDT_Channel
 from gdo.core.GDT_Creator import GDT_Creator
@@ -33,9 +34,14 @@ class GDO_YouTubeAbo(GDO):
         from gdo.youtube.module_youtube import module_youtube
         for abo in cls.table().select().exec().fetch_all():
             if channel := abo.gdo_value('yta_channel'):
-                key = 'msg_youtube_peeked' if origin_channel and channel.get_id() == origin_channel.get_id() else 'msg_youtube_shared'
-                await channel.send_text(key, (
-                    module_youtube.render_announcement(video, channel.get_render_mode()), video.gdo_val('yt_url')))
+                announcement = module_youtube.render_announcement(video, channel.get_render_mode())
+                if origin_channel and channel.get_id() == origin_channel.get_id():
+                    await channel.send_text('msg_youtube_peeked', (announcement, video.gdo_val('yt_url')))
+                elif origin_user:
+                    submitter = Render.bold(origin_user.render_name(), channel.get_render_mode())
+                    await channel.send_text('msg_youtube_shared_by', (submitter, announcement, video.gdo_val('yt_url')))
+                else:
+                    await channel.send_text('msg_youtube_shared', (announcement, video.gdo_val('yt_url')))
             elif user := abo.gdo_value('yta_user'):
                 key = 'msg_youtube_peeked' if not origin_channel and origin_user and user.get_id() == origin_user.get_id() else 'msg_youtube_shared'
                 await user.send(key, (
